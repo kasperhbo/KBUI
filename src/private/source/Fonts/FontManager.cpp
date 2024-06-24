72c2edd80b8ab4cc35c6af2fba0dd8d3df352e2b
Kasper de Bruin <k.debruin@hellebrekers.nl>
2024-06-24 21:09:43 +0200

    add jetbrains font

--- src/private/source/Fonts/FontManager.cpp
+++ src/private/source/Fonts/FontManager.cpp
@@ -6,25 +6,48 @@
 #include <VulkanWindow.h>
 #include <imgui.h>
 
-ImFont KBUI::Fonts::FontManager::LoadFont(const std::string &fontName, const std::string &fontPath) {
-    return ImFont();
-}
+namespace KBUI::Fonts {
+    Font& KBUI::Fonts::FontManager::LoadFont(const std::string &fontName, const std::filesystem::path &fontPath, const bool setAsDefault) {
+        auto io = ImGui::GetIO();
+        ImFontConfig config = GetDefaultConfig();
 
-void KBUI::Fonts::FontManager::LoadDefaultFont(float fontSize, float fontScale) {
-    auto io = ImGui::GetIO();
-    io.Fonts->Clear();
-    io.Fonts->ClearInputData();
-    io.Fonts->ClearTexData();
+        ImFont *imFont = io.Fonts->AddFontFromFileTTF(fontPath.c_str(), s_defaultFontSize * s_fontSizeIncreaseFactor, &config);
+        imFont->Scale = 1.0f / s_defaultFontScale;
 
-    ImFontConfig config;
+        std::shared_ptr<Font> font = std::make_shared<Font>();
+        font->Name = fontName;
+        font->Path = fontPath;
+        font->ImGuiFont = imFont;
+        font->SizeInPixels = config.SizePixels;
 
-    ImFontConfig baseConfig = ImFontConfig();
-    baseConfig.OversampleH = 1;
-    baseConfig.OversampleV = 1;
-    baseConfig.SizePixels = fontSize * fontScale;
+        s_Fonts[fontName] = font;
+        if(setAsDefault){
+            std::cout << "set default" << std::endl;
+            SetDefaultFont(fontName);
+        }
+        return *font;
+    }
 
-    ImFont *font = io.Fonts->AddFontDefault(&baseConfig);
-    font->Scale = 1.0f / fontScale;
+    void KBUI::Fonts::FontManager::LoadDefaultFont(float fontSize, float fontScale) {
+        auto io = ImGui::GetIO();
+        ImFontConfig config = GetDefaultConfig();
+        ImFont *font = io.Fonts->AddFontDefault(&config);
+        font->Scale = 1.0f / fontScale;
+        io.DisplayFramebufferScale = VulkanWindow::GetWindowScaleFactor();
+    }
 
-    io.DisplayFramebufferScale = VulkanWindow::GetWindowScaleFactor();
+    void FontManager::SetDefaultFont(const std::string &fontName) {
+        std::shared_ptr<Font> font = GetFont(fontName);
+        if(font != nullptr){
+            auto io = ImGui::GetIO();
+            io.FontDefault = font->ImGuiFont;
+        }else{
+            std::cerr << fontName << " not found" << std::endl;
+        }
+    }
+
+    std::shared_ptr<Font> FontManager::GetFont(const std::string &fontName) {
+        auto font = s_Fonts[fontName];
+        return font;
+    }
 }
--- src/public/headers/Fonts/FontManager.h
+++ src/public/headers/Fonts/FontManager.h
@@ -4,20 +4,52 @@
 
 #ifndef KBUI_FONTMANAGER_H
 #define KBUI_FONTMANAGER_H
+
 #include <string>
+#include <filesystem>
+#include <map>
+
 #include <imgui.h>
 
 namespace KBUI::Fonts {
+    struct Font {
+        std::string Name;
+        std::filesystem::path Path;
+        ImFont* ImGuiFont;
+        float SizeInPixels;
+    };
     class FontManager {
+
+    private:
         FontManager() = default;
 
         ~FontManager() = default;
+
     public:
-        static ImFont LoadFont(const std::string &fontName, const std::string &fontPath);
+        static ImFontConfig GetDefaultConfig(){
+            auto io = ImGui::GetIO();
+            io.Fonts->Clear();
+            io.Fonts->ClearInputData();
+            io.Fonts->ClearTexData();
+
+            ImFontConfig config;
+
+            ImFontConfig baseConfig = ImFontConfig();
+            baseConfig.OversampleH = 1;
+            baseConfig.OversampleV = 1;
+            baseConfig.SizePixels = s_defaultFontSize * s_defaultFontScale;
+
+            ImFont *font = io.Fonts->AddFontDefault(&baseConfig);
+            font->Scale = 1.0f / s_defaultFontScale;
+            return baseConfig;
+        }
+
+        static Font& LoadFont(const std::string &fontName, const std::filesystem::path &fontPath, const bool setAsDefaultFont);
         static void LoadDefaultFont(float fontSize = s_defaultFontSize, float fontScale = s_defaultFontScale);
+        static void SetDefaultFont(const std::string& fontName);
+        static std::shared_ptr<Font> GetFont(const std::string& fontName);
     private:
-        inline static float s_fontSize = 16.0f;
-        inline static float s_dpiWindowSizeFactor = 1.0f;
+        inline static std::map<std::string, std::shared_ptr<Font>> s_Fonts = {};
 #ifdef __APPLE__
         inline static float s_fontSizeIncreaseFactor = 2.0f;
         inline static float s_defaultFontSize = 16.0f;
